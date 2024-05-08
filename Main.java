b1ef11dc0189d800953e349fdec5eb6442591e91
Wallysson Oliveira <wallyssonryu@gmail.com>
2024-05-08 20:02:51 -0300

    Alteração na Classe Criatura, criação da função andarOutra

---
+++ Main.java
@@ -0,0 +1,25 @@
+import Criaturas.*;
+/** Main: documentação...
+ *
+ *
+ */
+public class Main {
+    // Documentação da outroMetodo
+    public int outroMetodo() {
+	return 2;
+    }
+
+    public static void main(String[] args) {
+	Criaturas criatura = new Criaturas(100);
+
+	criatura.andar("esquerda"); // Move
+	System.out.println(criatura.getPos()[0]);
+	criatura.andar("Esquerda"); // Retorna opção inválida
+	// para evitar isso podemos usar a outra função andar
+	criatura.andarOutra("Esquerda"); //Move
+	System.out.println(criatura.getPos()[0]);
+    }
+}
+
+// Fim do meu exemplo chato, qualquer coisa especifica de documentação do java
+// a gente pode ver junto do material da Esther, mas no geral gosot desta maneira
