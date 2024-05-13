83c5017fa157f8265c0eab5497153f92a5022d8d
besidJUbs <ventilarijuliana@gmail.com>
2024-05-13 10:24:35 -0300

    apaguei arkv chest

--- Mapa/Map.java
+++ Mapa/Map.java
@@ -3,19 +3,18 @@ import java.util.ArrayList;
 import Criatura.Criatura;
 
 public class Map {
-    ArrayList<NPC> npc =new ArrayList<NPC>();
+
+    ArrayList<npc> npc =new ArrayList<npc>();
     ArrayList<Criatura> monster =new ArrayList<Criatura>();
     ArrayList<Integer> chest =new ArrayList<Integer>(); //baú
 
 
-    static final int DIMENSIONx= 32;
-    static final int DIMENSIONy= 32;
+    static final int DIMENSION_X= 32;
+    static final int DIMENSION_Y= 32;
 
     
-    public Map (ArrayList<Integer> chest, ArrayList<NPC> npc,ArrayList<Criatura> monster ){
-        this.chest=chest;
-        this.npc=npc;
-        this.monster=monster;
+    public Map ( ){
+        
     }
 
     //getters and setters
@@ -24,15 +23,60 @@ public class Map {
         return chest;
     }
 
-    public ArrayList<NPC> GetNPC(){
+    public ArrayList<npc> GetNPC(){
         return npc;
     }
 
-    public ArrayList<Monster> GetMonster(){
+    public ArrayList<Criatura> GetMonster(){
         return monster;
     }
 
+    //add in the array
     
+    public void addMonster(Criatura monsterToAdd, ArrayList<Criatura> monster){
+        monster.add(monsterToAdd);
+    }
+
+    public void addChest(int chestToAdd, ArrayList<Integer> chest){
+        chest.add(chestToAdd);
+    }
+
+    public void addNPC(npc NPCToAdd, ArrayList<npc> npc){
+        npc.add(NPCToAdd);
+    }
+
+
+    //remove from array
+
+    public void RemoveMonster(Criatura monsterToRemove,ArrayList<Criatura> monster ){
+        for (int i=0; i< monster.size();i++){
+            if (monsterToRemove.equals(monster.get(i))){
+                monster.remove(i);
+              
+            }
+        }
+
+    }
+
+    public void RemoveNPC(npc npcToRemove, ArrayList<npc> npc){
+        for (int i=0; i< npc.size();i++){
+            if (npcToRemove.equals(npc.get(i))){
+                npc.remove(i);
+              
+            }
+        }
+
+    }
+
+    public void RemoveChest(Integer chestToRemove, ArrayList<Integer> chest){
+        for (int i=0; i< chest.size();i++){
+            if (chestToRemove.equals(chest.get(i))){
+                chest.remove(i);
+              
+            }
+        }
+
+    }
 
 
 
