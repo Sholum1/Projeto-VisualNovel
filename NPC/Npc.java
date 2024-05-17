190693ffd0ce0c358ad51529c34beea954806b0d
mur-rilo <jmurilomartins20@gmail.com>
2024-05-17 02:23:52 -0300

    Fiz o Personagem e o NPC (mas acho q trollei no npc)

---
+++ NPC/Npc.java
@@ -0,0 +1,28 @@
+package Npc;
+
+/*Fiz a classe normal, mas fui ver nql plano desenhado q a gnt fez que era pra ser uma classe abstrata, 
+  mas n entendi como ia ser isso se ia ter varios npcs, com falas diferentes, qqr coisa eu conserto dps,
+*/
+
+public class Npc {
+    private final String nome;
+    private final int[] pos = {0, 0};
+    private final String dialogo;
+
+    public Npc(String nome, String dialogo){
+        this.nome = nome;
+        this.dialogo = dialogo;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    
+    public String getDialogo(){
+        return dialogo;
+    }
+
+    protected void falar(String texto){
+        System.out.println(texto);
+    }
+}
