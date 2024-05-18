395388ae37f69068473bb741f7cd3f8e468e5e04
Maria Gabriela Lustosa <lustosa.mariagabriela@gmail.com>
2024-05-18 12:23:29 -0300

    algumas implementações em Itens

--- Item/Item.java
+++ Item/Item.java
@@ -35,6 +35,7 @@ public class Item {
             this.energia = energia;
             this.stackable = stackable;
         }
+
         // Se o item não estiver no enum, não pode ser colocado na mochila
 
         private boolean getConsumivel() {
@@ -63,45 +64,51 @@ public class Item {
     }
 
     // Classe que será a "mochila" do protagonista
-    protected void adicionarItem(Itens item, ArrayList<Item> mochila) { // Se precisar ser acessado fora do pacote, provável ter que mudar para public
+    public void adicionarItem(Itens item, ArrayList<Item> mochila) {
         // Se o item encontrado estiver presente no emun, pode ser guardado na mochila
         for (Itens i : Itens.values()) {
             if (item == i) {
                 for (int j = 0; j < mochila.size(); j++) {
-                    if (item.equals(mochila.get(j)) && item.getStackable()) {
+                    if (item.equals(mochila.get(j)) && item.getStackable()) { // Confere se já consta na mochila e se é stackable
                         // Adicionar o item
+                        // Se tudo ok, adiciona no array de itens que representa a mochila
                     }
                 }
-                // Confere se já consta na mochila e se é stackable
-                // Se tudo ok, adiciona no array de itens que representa a mochila
                 // Calcula a nova quantidade existente nela
             } else {
                 System.out.println("O item não pode ser guardado na mochila");
             }
         }
-
-        // Caso não possa ser adicionado, dispensar o item
     }
 
-    protected void removerItem(Itens item) {
-        // Se o item estiver dentro da mochila, pode ser removido
-
+    protected void removerItem(Itens item, ArrayList<Item> mochila) {
+        for (int i = 0; i < mochila.size(); i++) {
+            if (item.equals(mochila.get(i))) { // Se o item estiver dentro da mochila, pode ser removido
+                // Remover o item
+            }
+        }
     }
 
     protected void consumirItem(Itens item) {
-        // Se o item estiver presente na mochila e for "consumível"
-        if (item.getConsumivel()) {
-            // Calcular a cura e a energia obtidas em relação ao personagem
-            System.out.println("Giu tem mais " + item.getEnergia() + " pontos de energia");
-            System.out.println("A vida de Giu foi alterada em " + item.getVida() + " pontos");
-            // Talvez seja melhor implementar com o toString depois
-        }
+        for (int i = 0; i < mochila.size(); i++) {
+            if (item.equals(mochila.get(i))) { // Se o item estiver presente na mochila
+                if (item.getConsumivel()) { // Se o item for consumível
 
+                    // Calcular a cura e a energia obtidas em relação ao personagem
+                    System.out.println("Giu tem mais " + item.getEnergia() + " pontos de energia");
+                    System.out.println("A vida de Giu foi alterada em " + item.getVida() + " pontos");
+                    // Talvez seja melhor implementar com o toString depois
+                }
+            }
+        }
     }
 
     protected void verItens() {
         System.out.println("Dentro da mochila há: ");
-        // Percorrer o array que representa os itens guardados na mochila
+
+        for (int i = 0; i < mochila.size(); i++) {
+            System.out.println(mochila.get(i));
+        }
     }
 
     protected void dispensarItem(Itens item) { // Talvez esse método não precise existir
