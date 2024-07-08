023fce9ab39e8a3eeae76f50c04c9869ddfc7581
besidJUbs <141164323+besidJUbs@users.noreply.github.com>
2024-07-08 20:53:50 -0300

    Add files via upload

---
+++ pseudocodigobemlixinho.java
@@ -0,0 +1,206 @@
+public class pseudocodigobemlixinho {
+    //quadro1_lua.png
+    
+    System.out.println("giu ouve o som das ondas e olha pra lua suplicando q a devolva luna, sua peixe q se foi dias atrás.giu sabe que peixes vivem pouco, e quando a viu cansada no seu aquário ja previa o pior, mas não tão cedo.");
+    System.out.println("a lua começa a cantar: a ninar, ouça as ondas do mar...deixe o som te guiar...a luz da lua te leva...de volta ao mar.");
+
+
+    //mapa_geral.png
+
+    System.out.println("giu acorda assustada dentro da densa água, e tenta ir pra superficie, mas nunca acaba. uma pequena pérola iluminada a segue.");
+    System.out.println("lua: giu do que adianta fugir da realidade tonteante? se estamos no mar, temos que nadar em frente ao futuro.eu te trouxe ao fundo do oceano, no mais remoto bloco de água.sou a lua, e te trouxe pra acha-la novamente.");
+
+    System.out.println("as suas conversas com a lua sobre luna geraram frutos! você deseja:")
+    
+    if ( j.button: nadar para a superficie){
+
+    System.out.println("mesmo que tentemos fugir, mesmo com medo do novo mundo, aprendemos a respirar melhor embaixo d'agua enquanto tentamos.")
+
+}
+    
+    if ( j.button: nadar para a superficie){
+
+        System.out.println("vamos, vamos em frente!")
+
+    }
+    if ( j.button: nadar para a superficie){
+
+        System.out.println("eu te acompanheirei ate em seus momentos de desespero")
+    
+    }
+    if ( j.button: nadar para a superficie){
+
+        System.out.println("vamos, vamos em frente!")
+    
+    }
+
+
+    if( j.button: seguir em frente){
+        //mapa_geral.png
+
+        System.out.println("nadando pelo mar giu vê um peixe rápido, vermelho e preto.
+um estalo de memórias com Luna volta como um grande peso em sua cabeça.
+seria ela? nadando livremente no mar?");
+        System.out.println("Giu: -Ei! Volte aqui!");
+        //flanders.png
+        System.out.println("o peixe vai em direção a Giu, mas quanto mais se aproxima, mais vê que não é nada parecido com Luna.");
+        System.out.println("Flanders: oq quer?");
+        System.out.println("Giu: estou perdida nesse grande mar!");
+        System.out.println("Flanders: e oq tenho haver com isso?");
+        System.out.println("Giu: -pensei q poderia me ajudar ....");
+        System.out.println("Flanders, o peixe perdido seu cardume, respira fundo e vê Giu como uma oportunidade de se distrair das fortes correntes marítimas que teria q enfrentar sozinho.");
+        System.out.println("Flanders: vamos nadar juntos!");
+
+        j.button: aceitar amizade;
+
+        
+        //mapa_carangueijo_boss_corais.png
+        System.out.println("Flanders e Giu nadam pelas águas pouco iluminadas...tentando achar uma saída pra Giu. Mas Flanders está tão perdido quanto.");
+        System.out.println("O azul do mar se mistura com uma sombra com quatro patas e um pequeno chapéu na cabeça.");
+        //carangueijo_villain.png
+        System.out.println("Carangueijo Marujo:Quem são vocês? Não ultrapassem esses corais!");
+        System.out.println("Giu: Estamos procurando a costa!");
+        System.out.println("Huh...eu ja andei pela costa..naveguei em grandes navios... em meus tempos de pirata. Deixe me apresentar: eu sou o Carangueijo Marujo da grande Garantia Vermelha.
+E eu sei como ir para a costa! Mas só é possível se ultrapassar estes corais, que pertencem à mim! Não deixarei estranhos passarem!");
+        System.out.println("Giu tem esperança após descobrir que esta no sentido certo da costa...");
+        System.out.println("Giu: Só precisamos rapidamente nadar sob seus corais, não iremos causar nenhum problema! Contorna-los gastaria muito tempo!");
+        System.out.println("Carangueijo Marujo:O tempo é uma ilusão nesse vasto mar...vocês terão...que LUTAR!");
+
+
+        if( j.button: FUGIR){
+            MEDO+=100;
+            System.out.println("flanders olha para Giu em acordo e os dois nadam rapidamente na direção oposta, ouvindo os gritos do Carangueijo Marujo e deixando a oportunidade de chegar a costa mais rápido pra tras.");
+            System.out.println("após horas nadando, bolhas se aproximam do peixe e da humana e uma flecha de água os atravessa!");
+            //cardume_sardinhas.png
+
+            System.out.println("Giu: Um cardume...!");
+            System.out.println("Flanders: de sardinhas! ");
+            System.out.println("essa seria uma das visões mais lindas q ja tinha visto... peixes em conjunto rodopiando contra o azul do mar e as bolhas surgindo como as nuvens da terra firme.
+");
+            //(%felicidade aumenta)
+            System.out.println("em meio ao rápido cardume uma sardinha apressada e atrasada se aproxima");
+            //sardy.png
+            System.out.println("SARDY:eles são tão rápidos e sabem nadar tão perfeitamente... é tão dificil acompanhar esse ritmo. na terra firme é parecido de alguma forma?");
+            System.out.println("GIU:os carros correm rápido, o relógio corre rápido, as pessoas correm contra o tempo em seus carros, é tudo uma grande e rápida competição.");
+            System.out.println("SARDY:por isso veio ao mar? pra fugir disto? aqui não é tão diferente!");
+            System.out.println("Giu: -estou procurando o caminho de casa, apesar de tudo.!");
+            System.out.println("SARDY:-já vi humanos em suas grandes embarcações andando por aqui. posso te ajudar a achar uma! Sou a Sardy, deixe eu te ajudar a retornar a casa enquanto eu fujo da minha..");
+            System.out.println("Sardy, Flanders e Giu vão à baia, onde as águas são mais rasas...");
+
+            //quadro_barco_de_humanos.png
+            System.out.println("BARCO AVISTA!
+TENTE INVADIR UM BARCO!");
+
+            j.button: INVADIR;
+            System.out.println("a grande pressão da massa de água ao redor do barco empurra os três pra bem longe.");
+            System.out.println("Giu:-lua...me ajude...não sei como lidar com o mar que me colocastes....");
+            
+            j.button: toque a lua!;
+
+            //assassinato_humanos_barco.png
+
+            System.out.println("uma grande luz cega os motores e eles param de funcionar.");
+
+            j.button: ira vermelha-mata todos instantaniamente -//(%tristeza sobe)
+            j.button: brilho do desmaio-inimigos desmaiados por 5 segundos!  digite desmaio para acionar o poder novamente //(%medo sobe)
+            
+            
+            //mapa barco dura uns 2min.. mas o player n sabe disso.. ent ele pode acabar matando td mundo por achar q n tem coo passar do mapa
+
+            funcion1;
+
+
+        }
+        if( j.button: BATALHAR ){
+            ENERGIA-=100;
+
+            System.out.println("carangueijo se rende no final e vira servo de navio de Giu.");
+
+            //mapa_polvo_boss_barcos_quebrados.png
+            System.out.println("pelos corais, o Marujo vai apresentando varias peças úteis para contruir um barco:");
+            mostrar no mapa:
+            baú de ouro
+            vela de naviu
+            base da balsa
+            tomo do barco
+
+            System.out.println("ao agarrar o tomo do barco, uma força o puxa de volta e uma grande sombra se põe de frente a eles.
+            um polvo gingante os surpreende:");
+            //polvo_villain.png
+            System.out.println("POLVO: -SE PREPARE PRA MORRER!");
+
+            j.button: BATALHAR;
+            System.out.println("polvo com 5 de hp");
+            System.out.println("POLVO: o mar te assombrou, e a Lua pode ate tentar te enganar, mas todos nos sabemos como voxê nunca mais achará Luna!
+            Pode passear pelas águas que ela nadava, e os corais apenas vão acenar a você com grande pesar!
+            PENA. Por uma tristeza que nunca será curada, este buraco ira consumir todo o seu coração, até corroer as bordas do seu ser, pois ela NUNCA voltará!
+            vocês nunca mais nadarão juntas. pode ficar 100 anos neste mar esperando, e ficará 100 eternos anos sozinha, na companhia de memórias do passado!
+            Qual o sentido desta viagem Giu? Se sente capaz de trazê-la magicamente a vida e q ela venha correndo das areias em direção a você? Pare de ser soberba!
+            Se sente em tanto controle da vida? Então apenas me mate. E eu virarei apenas memórias que voarão como o pó do que um dia foi o corpo dela!");
+            
+
+            if(j.button: MATAR POLVO){
+                funcion1;
+            }
+
+            if(j.button:O DEIXAR RENDIDO){
+                System.out.println("polvo: covarde! me dê o mesmo destino dela! pise em meu corpo sem vida assim como pisa no dela, enquanto tenta a trazer de volta!
+                você não engana a mim! você não quer chegar a costa, só quer fugir e afundar cada vez nesse mar.
+                me mate e me deixe afundar como ela!");
+
+                System.out.println("tomada de raiva por invadir suas memorias de Luna com cenas horriveis e por desrespeitá-la mesmo não estando em vida, Giu ergue a espada e perfura o coração do polvo... que aos poucos afunda no mar, até se perder de vista.
+
+
+                após matar alguem com as próprias mãos, Giu cai em lágrimas. afinal, esta ali nadando em meio a saudade da perda de Luna, enquanto acaba de tirar a vida de um polvo.
+                por que decidiu matar alguem?
+                por que eta tão injusto a vida tirar Luna de si, mas agora, em meio a impulsividade, fez o mesmo?
+
+                Vendo os grandes olhos cheios de medo e tristeza, Flanders, Marujo e a Lua constroem uma pequena balsa e carregam Giu ao encontro do céu e do mar.");
+
+
+                function1;
+            }
+
+
+
+
+            
+
+        }
+
+        funcion1(
+            //cena_final_mar_ondas.png
+        System.out.println("finalmente respirando ar, após um longo tempo, Giu se vê deslocada.
+                e navega pelo mar...
+                e navega pelo mar.....
+
+                Grandes ondas surgem!
+
+                Uma onda de 100metros de comprimento se aproximam da pequena embarcação e destroem tudo sem piedade.
+                Ao se passarem 10 segundos, mais uma, e logo após mais uma.
+                Giu não consegue nem respirar no intervalo que essas ondas a atingem. Ela se agarra a pedaços da madeira quem um dia foram o barco e enfrenta uma nova onda.
+                Após uma semana, as ondas de 100 metros continuam vindo, mas mais espaçadas.
+                Quando elas chegam elas destroem completamente tudo, mas no pequeno intervalo entre uma e outra, há tempo pra respirar e organizar a mente.
+                Após um tempo... as ondas pararam de vir e Giu pôde reconstruir seu barco com a ajuda de seus novos amigos, até que, em meio a calmaria do mar, uma grande onda surge no horizonte.
+
+                É o aniversário de Luna.
+
+                Tudo se destrói novamente, e tudo oq sobram são destroços do belo barco que tinha em conjunto com Luna, o véu é o dia em a carregou no pequeno saco com água da escola até seu aquário. O mastro a trás de volta ao dia que andavam pela costa do mar procurando conchas.
+
+                o luto vem em ondas, e destrói o lindo navio que cultivaram juntas, e todos os destroços do navio boiando no mar após uma grande onda passar te lembram do quão lindo era estar ao lado de Luna, mas que agora são apenas antigas memórias, que Giu se agarra para não afundar no fundo do oceano.
+
+                Mas dessa vez Giu já sabia, a onda passaria, a enxarcaria, destruiria o navio todo denovo, mas iria passar, as roupas iram secar eventualmente, e ela juntará novamente as memórias, mesmo que os pedaços não de encaixem mais, e viverá confortavelmente sabendo que em um ponto do passado, pôde dividir este lindo navio com Luna.
+
+                ");
+
+                //quadro1_lua.png
+
+                System.out.println("Após dormir sob os destroços do barco, a lua ao lado de Giu sobe aos céus da noite, que antigamente era apenas escuridão e um grande brilho preenche o céu.
+                enfim Giu acorda na areia da praia, com os olhos marejados.");
+        );
+
+
+    }
+
+
+}
