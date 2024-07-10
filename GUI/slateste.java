f2f47aabfa08374e95771c590c5a60ac2e716a8b
besidJUbs <ventilarijuliana@gmail.com>
2024-07-09 23:38:59 -0300

    aaaa

--- GUI/slateste.java
+++ GUI/slateste.java
@@ -1,20 +1,29 @@
 package GUI;
 
+import java.awt.event.ActionEvent;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class slateste extends Jframe implements ActionListener {
-
-    MyFrame(){
-        JButton button = new JButton("toque na lua");
+public class slateste extends JFrame implements ActionListener {
+    JButton button;
+    slateste(){
+        button = new JButton("toque na lua");
         button.setBounds(200,100,100,50);
+        button.addActionListener(this);
 
 
-        this.setDefaultCloseOperation(Jframe.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500,500);
         this.setVisible(true);
         this.add(button);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource()==button){
+
+        }
     }
     
 }
