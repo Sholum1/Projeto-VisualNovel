50d81dc4f7d0e3810e748c23e7a4884db84f2f09
besidJUbs <ventilarijuliana@gmail.com>
2024-07-10 16:44:51 -0300

    tela incial

--- GUI/TelaInicial.java
+++ GUI/TelaInicial.java
@@ -16,7 +16,7 @@ import java.awt.*;
 public class TelaInicial {
     public void rodaTelaInicial(MyFrame frame) {
 
-        ImageIcon mapaGeral = new ImageIcon("Assets/mapa geral.png");
+        ImageIcon mapaGeral = new ImageIcon("mapa_geral_.png");
 
         Border border = BorderFactory.createLineBorder(new Color(255, 176, 120), 12, false);
 
