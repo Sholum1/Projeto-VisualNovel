61c1693f20dabb1bfb46e66f5536fecd958e8a9f
Wallysson Oliveira <wallyssonryu@gmail.com>
2024-07-12 00:39:12 -0300

    Terminei o Boss

--- GUI/ProgressBar.java
+++ GUI/ProgressBar.java
@@ -3,44 +3,26 @@ package GUI;
 import java.awt.*;
 import javax.swing.*;
 public class ProgressBar {
-
-
     JFrame frame = new JFrame();
     JProgressBar bar= new JProgressBar(5,100);
-    ProgressBar(){
 
+    ProgressBar() {
         bar.setValue(100);
         bar.setBounds(0, 0, 420, 50);
         bar.setStringPainted(true);
         bar.setFont(new Font("Poppins", Font.BOLD,25));
         bar.setForeground(Color.green);
         bar.setBackground(Color.white);
-        
-
 
         frame.add(bar);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420, 420);
         frame.setLayout(null);
         frame.setVisible(true);
-        fill();
+        // fill(); O Fill fica onde a gente roda a barra com bar.fill()
     }
-    public void fill(){
-        int counter =100 ;
-        while(counter >=5){ //qnd da 5 de hp o boss se rende
-
-            bar.setValue(counter);
-
-            try{
-                Thread.sleep(50);
-            }
-            catch(InterruptedException e){
-                e.printStackTrace();
-            }
-            counter-=1;
-
-            }
-        bar.setString("Eu me rendo!");
+    // Progresso = (vida atual do monstro - dano)*100/vida total
+    public void fill(int progresso){
+	bar.setValue(progresso);
     }
-    
 }
