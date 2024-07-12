91a09109d4949f382bd02bc4efeea570df0344fb
Maria Gabriela Lustosa <lustosa.mariagabriela@gmail.com>
2024-07-12 05:03:38 -0300

    sla qq rolou no commit anterior :(

--- Main.java
+++ Main.java
@@ -14,11 +14,5 @@ public class Main {
 	MyFrame frame =  new MyFrame();
 	TelaInicial tela = new TelaInicial();
 	tela.rodaTelaInicial(frame);
-<<<<<<< HEAD
-	//Mochila mochila = new Mochila();
-	//mochila.rodaConversa(frame);
-
-=======
->>>>>>> 5866f810a15bdc520b6fee0222680f870685aaab
     }
 }
